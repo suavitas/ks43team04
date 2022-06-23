7bcff4d984d9cc34483e0293552a0691f3dc72fb
YongMinMax <fry2you@naver.com>
2022-06-23 17:34:12 +0900

    [20220623]

---
+++ ks43team04/src/main/java/ks43team04/mapper/BillMapper.java
@@ -0,0 +1,15 @@
+package ks43team04.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ks43team04.dto.Bill;
+
+@Mapper
+public interface BillMapper {
+	
+	
+	public List<Bill> getBillList();
+	
+}
