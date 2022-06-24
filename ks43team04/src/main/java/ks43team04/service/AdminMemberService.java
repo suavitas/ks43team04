40e9ce9eaa105e8bdca8b64ecc419a98f30bbec9
h220101 <ssw3417@naver.com>
2022-06-24 10:37:20 +0900

    [0624] adminmember관련파일, main.css 수정

--- ks43team04/src/main/java/ks43team04/service/AdminMemberService.java
+++ ks43team04/src/main/java/ks43team04/service/AdminMemberService.java
@@ -5,34 +5,40 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ks43team04.dto.Laundry;
 import ks43team04.dto.Member;
 import ks43team04.mapper.AdminMemberMapper;
+
 
 @Service
 @Transactional
 public class AdminMemberService {
-
+	
 	private final AdminMemberMapper adminMemberMapper;
-
+	
 	public AdminMemberService(AdminMemberMapper adminMemberMapper) {
 		this.adminMemberMapper = adminMemberMapper;
 	}
-
-	// 관리자페이지 회원 상세정보 조회
+	
+	//관리자페이지 세탁소 점주 정보조회
+	public List<Laundry> getLaundryList() {		
+		   List<Laundry> LaundryList = adminMemberMapper.getLaundryList();		
+		return LaundryList;
+	}
+	
+	//관리자페이지 회원 상세정보 조회
 	public Member getMemberInfoById(String memberId) {
 		return adminMemberMapper.getMemberInfoById(memberId);
 	}
-
-	// 관리자페이지 관리자 권한 회원가입
-	public int addMemberAdmin(Member member) {
+	//관리자페이지 관리자 권한 회원가입
+	public int addMemberAdmin(Member member) {		
 		int addMemberAdmin = adminMemberMapper.addMemberAdmin(member);
-
-		return addMemberAdmin;
-	}
-
-	// 관리자페이지 전체 회원 목록 조회
-	public List<Member> getAdminMemberList() {
-		List<Member> adminMemberList = adminMemberMapper.getAdminMemberList();
+		
+		return addMemberAdmin;	
+	}	
+	//관리자페이지 전체회원 목록 조회
+	public List<Member> getAdminMemberList() {		
+		   List<Member> adminMemberList = adminMemberMapper.getAdminMemberList();		
 		return adminMemberList;
 	}
 }
--- ks43team04/src/main/java/ks43team04/service/MemberService.java
+++ ks43team04/src/main/java/ks43team04/service/MemberService.java
@@ -3,6 +3,7 @@ package ks43team04.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ks43team04.dto.Laundry;
 import ks43team04.dto.Member;
 import ks43team04.mapper.MemberMapper;
 
@@ -10,26 +11,27 @@ import ks43team04.mapper.MemberMapper;
 @Transactional
 public class MemberService {
 	private final MemberMapper memberMapper;
-
+	
 	public MemberService(MemberMapper memberMapper) {
 		this.memberMapper = memberMapper;
 	}
 
-	/*
-	 * 세탁소 상세정보 조회 public Laundry getLaundryInfo(String memberId) { return
-	 * memberMapper.getLaundryInfo(memberId); }
-	 */
-
-	// 회원 상세정보 조회
+	
+	//일반, 무인 점주 회원가입
+	public int addMemberLaundry(Laundry laundry) {		
+		int addMemberLaundry = memberMapper.addMemberLaundry(laundry);		
+		return addMemberLaundry;	
+	}
+		
+	//회원 상세정보 조회
 	public Member getMemberInfoById(String memberId) {
 		return memberMapper.getMemberInfoById(memberId);
 	}
-
-	// 고객, 관리자 회원가입
-	public int addMember(Member member) {
-		int addMember = memberMapper.addMember(member);
-
-		return addMember;
-	}
-
+	
+	//고객, 관리자(메인화면) 회원가입 
+	public int addMember(Member member) {		
+		int addMember = memberMapper.addMember(member);	
+		return addMember;	
+	}		
+	
 }
