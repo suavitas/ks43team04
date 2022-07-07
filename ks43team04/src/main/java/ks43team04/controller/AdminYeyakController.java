27399613705ba7e87ade18dc6924c32ef5de7197
choiwooyoung <csij5164@naver.com>
2022-07-07 14:01:59 +0900

    [2022-07-07] 무인 세탁소 세탁기 예약현황 수정 AdminYeyakController.java YeyakMapper.xml YeyakService.java yeyakStatus.html

--- ks43team04/src/main/java/ks43team04/controller/AdminYeyakController.java
+++ ks43team04/src/main/java/ks43team04/controller/AdminYeyakController.java
@@ -1,10 +1,13 @@
 package ks43team04.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ks43team04.dto.MuinMachine;
 import ks43team04.service.YeyakService;
 
 @RequestMapping("/admin")
@@ -19,7 +22,12 @@ public class AdminYeyakController {
 
 	@GetMapping("/yeyakStatus")
 	public String muinYeyak(Model model) {
-
+		List<MuinMachine> muinMachineList = yeyakService.muinMachineList();
+		
+		System.out.println(muinMachineList);
+		
+		model.addAttribute("muinMachineList", muinMachineList);
+		
 		return "/admin/yeyak/yeyakStatus";
 	}
 
