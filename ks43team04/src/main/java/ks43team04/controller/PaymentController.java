3d4d295f57b09789a268539d487372f61e345ac3
leehyeonoo <vgusdn729@gmail.com>
2022-07-13 12:31:18 +0900

    [0713-1230] 결제페이지 경로 수정 및 카카오api 연동 준비 중

--- ks43team04/src/main/java/ks43team04/controller/PaymentController.java
+++ ks43team04/src/main/java/ks43team04/controller/PaymentController.java
@@ -9,47 +9,121 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ks43team04.dto.Bill2;
 import ks43team04.dto.Laundry;
 import ks43team04.dto.Member;
 import ks43team04.dto.MemberLevel;
 import ks43team04.dto.PayMember;
+import ks43team04.service.BillService;
 import ks43team04.service.LaundryService;
 import ks43team04.service.MemberService;
 import ks43team04.service.PaymentService;
 
+@RequestMapping("/user")
 @Controller
 public class PaymentController {
 	
 	private final LaundryService laundryService;
 	private final MemberService memberService;
 	private final PaymentService paymentService;
+	private final BillService billService;
 	
 	
-	public PaymentController(LaundryService laundryService, MemberService memberService, PaymentService paymentService) {
+	public PaymentController(LaundryService laundryService, MemberService memberService, PaymentService paymentService, BillService billService) {
 		this.laundryService = laundryService;
 		this.memberService = memberService;
 		this.paymentService = paymentService;
+		this.billService = billService;
 	}
 	
-	@PostMapping("/user/payment")
-	public String payment(@RequestParam(name = "laundryName") String laundryName,
-							@RequestParam(name = "goodsName") String goodsName
-							,Model model) {
+	@GetMapping("/myPagePayment2")
+	public String myPagePayment2(@RequestParam(name = "currentPage", required = false, defaultValue = "1") int currentPage
+								,Model model,HttpSession session) {
 		
-		List<Laundry> laundryGoodsNameAndPrice = laundryService.getLaundryGoodsNameAndPrice(laundryName, goodsName);
+		String sessionId = (String) session.getAttribute("SID");
+		String sessionName = (String) session.getAttribute("SNAME");
 		
-		System.out.println(laundryName+" <----------세탁소이름");
-		System.out.println(goodsName+" <----------상품이름");
-		System.out.println(laundryGoodsNameAndPrice+" <----------laundryGoodsNameAndPrice 리스트값");
+		Member member = memberService.getMemberInfoById(sessionId);
+		//세션 이름 저장
+		model.addAttribute("sessionName", sessionName);
 		
-		model.addAttribute("laundryGoodsNameAndPrice", laundryGoodsNameAndPrice);
+		String memberId = sessionId;
+		//rowCount > id 결제건수
+		int rowCount = billService.getBillCount2(memberId);
+		
+		model.addAttribute("rowCount", rowCount);
+	
+		System.out.println(sessionId+"<------- sessionId");
+		System.out.println(sessionName+"<------- sessionName");
+		System.out.println(memberId+"<------- memberId");
+		System.out.println(rowCount+"<------- rowCount");
+		//페이징처리시작
+		Map<String, Object> resultMap = billService.billPage(currentPage, session);
+		
+		System.out.println(resultMap.get("billPage"));
+		
+		model.addAttribute("resultMap", resultMap);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("billPage", resultMap.get("billPage"));
+		model.addAttribute("lastPage", resultMap.get("lastPage"));
+		model.addAttribute("startPageNum", resultMap.get("startPageNum"));
+		model.addAttribute("endPageNum", resultMap.get("endPageNum"));
+		
+		return "/member/myPagePayment2";
+	}
+	
+	@PostMapping("/myPagePayment2")
+	public String myPagePayment2(Bill2 bill) {
+		
+		System.out.println(bill);
+		
+		billService.addBill2(bill);
+		
+		return "redirect:/user/myPagePayment2";
+	}
+	
+	
+	
+	@PostMapping("/paymentComplete")
+	public String paymentComplete(Bill2 bill) {
+		
+		System.out.println(bill);
+		
+		billService.addBill2(bill);
+		
+		return "/user/payment/paymentComplete";
+	}
+	
+	@GetMapping("paymentComplete")
+	public String paymentComplete() {
+		return "user/payment/paymentComplete";
+	}
+	
+	@GetMapping("payment")
+	public String payment(Model model, HttpSession session) {
+		
+		String sessionId = (String) session.getAttribute("SID");
+		String sessionName = (String) session.getAttribute("SNAME");
+		String sessionLevel = (String) session.getAttribute("SLEVEL");
+		
+		
+		PayMember paymember = paymentService.getPayMInfo(sessionId);
+		
+		model.addAttribute("title", "마이페이지");
+		model.addAttribute("sessionName", sessionName);
+		model.addAttribute("paymember", paymember);
+		model.addAttribute("sessionLevel", sessionLevel);
+		
+		MemberLevel memberlevel = paymentService.getMemberLevel(sessionLevel);	
+		model.addAttribute("memberlevel", memberlevel);
 		
 		return "user/payment/payment";
 	}
 	
-	@PostMapping("/user/yeyakPayment")
+	@PostMapping("payment")
 	public String yeyakPayment(@RequestParam(name = "laundryName") String laundryName,
 							@RequestParam(name = "goodsName") String goodsName,
 							@RequestParam(name = "bill") String bill,
@@ -74,41 +148,12 @@ public class PaymentController {
 		model.addAttribute("mEmail", mEmail);
 		model.addAttribute("sessionId", sessionId);
 		
-		return "user/payment/yeyakPayment";
-	}
-	
-	@GetMapping("/user/paymentComplete")
-	public String paymentComplete() {
-		return "user/payment/paymentComplete";
-	}
-	@GetMapping("/user/payment")
-	public String payment(Model model, HttpSession session) {
-
-		String sessionId = (String) session.getAttribute("SID");
-		String sessionName = (String) session.getAttribute("SNAME");
-		String sessionLevel = (String) session.getAttribute("SLEVEL");
-		
-
-		PayMember paymember = paymentService.getPayMInfo(sessionId);
-
-		model.addAttribute("title", "마이페이지");
-		model.addAttribute("sessionName", sessionName);
-		model.addAttribute("paymember", paymember);
-		model.addAttribute("sessionLevel", sessionLevel);
-		
-		
-		
-		MemberLevel memberlevel = paymentService.getMemberLevel(sessionLevel);	
-		model.addAttribute("memberlevel", memberlevel);
-		
 		return "user/payment/payment";
 	}
 	
-
 	@GetMapping("/admin/goodsPrice")
 	public String eachGoodsPriceList(@RequestParam(name = "currentPage", required = false, defaultValue = "1") int currentPage,
 	Model model, HttpSession session) {
-		
 		
 		Map<String, Object> resultMap = laundryService.eachGoodsPriceList(currentPage);
 		
@@ -129,5 +174,4 @@ public class PaymentController {
 		
 		return "admin/goodsPrice";
 	}
-
 }
